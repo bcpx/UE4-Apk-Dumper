a618200659f63c674c0d8c5644e7902e5aba9762
Sk Md Asib <98967932+Real-Ascarre@users.noreply.github.com>
2022-05-13 09:49:06 +0530

    Update Offsets64.h

--- SDKDumper/app/src/main/jni/Offsets64.h
+++ SDKDumper/app/src/main/jni/Offsets64.h
@@ -5,7 +5,7 @@
 #ifndef SDKGENERATOR_OFFSETS64_H
 #define SDKGENERATOR_OFFSETS64_H
 
-bool isFindGNames = false, isFindGUObject = false, isFindGWorld = false, isStringDump = false, isObjectsDump = false, isSdkDump = false, isSdkWDump = false, isNew = false, isPubgM = false;
+bool isStringDump = false, isObjectsDump = false, isSdkDump = false, isSdkWDump = false, isNew = false, isPubgM = false;
 
 string OutPutPathDirectory("/sdcard/DumpedGames/");
 
